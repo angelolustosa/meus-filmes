83cd021020ffe8bf691bff26beb8606c4b4348b4
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-07-08 22:06:25 -0300

    Carregar os dadsos da api dos filmes favoritos e mocar um card

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const options = {
     method: 'GET',
     headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0OGU3MDFkYjNmNTUyZTBhNTFjMDlkNDMxMzdiZDI3MCIsIm5iZiI6MTY4ODczMDA1NC44NzgsInN1YiI6IjY0YTdmOWM2OTY1MjIwMDExZGYwOGU3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YhX8YDb0OF8ovacEzdWjUTSWr0xZLaZOItyxsnzgVMI'
     }
-}
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
 
-fetch(`https://api.themoviedb.org/3/authentication`, options)
+
+/* fetch(`https://api.themoviedb.org/3/authentication`, options)
     .then(response => response.json())
     .then(data =>{
         console.log(data);
-    })
+    }) */
